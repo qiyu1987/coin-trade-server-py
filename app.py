8b430d0ac9278d213a43a8ec2f6daebb2da772fa
qiyu1987 <qiyu1987@gmail.com>
2019-11-20 09:50:20 +0100

    pipenv

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
 
 
-
+app.debug = True
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
