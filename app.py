c99a9e6b57df1f5bcdd2518aef77868092278a5a
qiyu1987 <qiyu1987@gmail.com>
2019-11-20 08:43:21 +0100

    home signup route

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello, World!"
-    
+    return render_template("home.html")
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
